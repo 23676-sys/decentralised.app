cb528ab26718bd4be47b58c7a35f9466eaf20cbf
anna <23676@tgs.school.nz>
2026-06-08 09:39:57 +1200

    Add files via upload

---
+++ 01_yes_no_v2.py
@@ -0,0 +1,26 @@
+# funstions go here
+def yes_no(question):
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        
+        elif response == "no" or response == "n":
+            return "no"
+        
+        else:
+            print("Please enter yes or no")
+
+
+# main routine goes here
+while True: 
+    want_instructions = yes_no("Do you want to read these insturctions? ").lower()
+    
+    if want_instructions == "yes" or "y":
+        print("Instructions go here")
+    
+print("programe continues...")
+print()
+
