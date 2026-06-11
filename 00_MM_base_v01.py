eff100f2beb12101a7afef823cdc5626b1cfe2bf
anna <23676@tgs.school.nz>
2026-06-11 12:10:13 +1200

    Add files via upload

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -1,40 +1,81 @@
-#functions go here
+# functions go here
+
+
+# Checks user has enterd yes / no to a question 
 def yes_no(question):
 
     while True:
         response = input(question).lower()
-        
+
         if response == "yes" or response == "y":
             return "yes"
+        
         elif response == "no" or response == "n":
             return "no"
-        else: 
+        
+        else:
             print("Please enter yes or no")
 
-# Cehcks user has entered yes / no to a question 
-# main routine starts here
+# checks that user response is not blank
+def not_blank(question):
+    
+    while True:
+        response = input(question)
+
+        if response == "":
+            print("Sorry, this can't be blank. Please enter your name.")
+        else:
+            return response
+
+
+# checks users enter an integer to a given question
+def num_check(question):
+
+    while True: 
+
+        try: 
+            response = int(input(question))
+            return response
+        except ValueError:
+            print("Please enter a valid integer.")
+
+
+
+# main routine starts here 
 
 # set maximum number of tickets below
 MAX_TICKETS = 3
-tickets_sold = 0
-
-# Ask user if they want to see the instructions
-want_instructions = yes_no("Do you want to read the instruction? ")
-
-
-
-# loop to sell tickets
 tickets_sold = 0 
+# Ask user if they want to read instructions
+want_instructions = yes_no("Do you want to read these insturctions? ").lower
+
+if want_instructions == "yes" or want_instructions == "y":
+    print("Instructions go here")
+
+print()
+# loop to sell tickets 
+ 
 while tickets_sold < MAX_TICKETS:
-    name = input("Please enter your name or 'xxx' to quit: ")
+    name = not_blank("Please enter your name (or 'xxx' to quit) ")
 
     if name == 'xxx':
         break
 
+    age = num_check("Age: ")
+
+    if 12 <= age <= 120: 
+        pass
+    elif age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    else: 
+        print("?? That looks like a typo, please try again. ")
+        continue
+
     tickets_sold += 1
-# output number of tickets sold
-if tickets_sold == MAX_TICKETS:
-    print("Congratulations you have sold all the tickets ")
-else: 
-    print("You have sold {} ticket/s. There is {} ticket/s"
-           "remaining").format(tickets_sold, MAX_TICKETS - tickets_sold)
+
+    # output number of tickets sold
+    if tickets_sold == MAX_TICKETS:
+        print("Congratulations you have sold all the tickets ")
+    else:
+        print("You have sold {} ticket/s. There is {} ticket/s")
